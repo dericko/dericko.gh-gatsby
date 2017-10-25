5d0edd1e1ff00f98192ae39747a267993c9d973b
Derick Olson <olsonderick@gmail.com>
2017-10-25 19:44:18 -0400

    add sass

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 import logo from '../assets/logo.png'
 
-import styles from './styles/header.module.css'
+import styles from './styles/header.module.scss'
 
 export default () => (
   <header className={styles.header}>
