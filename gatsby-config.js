96521bb06285763041d41510a794ebfd8ca7df75
Derick Olson <olsonderick@gmail.com>
2017-10-25 14:52:21 -0400

    update head

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Derick Olson`,
+    description: `Hi there, my name's Derick. I'm a student of languages (imperative, functional, figurative, semanto-phonetic).`,
+    keywords: `derick, olson, olsonderick, dericko, writing, software`
   },
   plugins: [`gatsby-plugin-react-helmet`],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -34,31 +34,46 @@ const Header = () => (
   </div>
 )
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+const TemplateWrapper = ({ children, data }) => {
+  const { title, description, keywords } = data.site.siteMetadata
+  return (
+    <div>
+      <Helmet
+        title={title}
+        meta={[
+          { name: 'description', content: description },
+          { name: 'keywords', content: keywords },
+        ]}
+      />
+      <Header />
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0,
+        }}
+      >
+        {children()}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
 }
 
 export default TemplateWrapper
+
+export const query = graphql`
+  query LayoutQuery {
+    site {
+      siteMetadata {
+        title
+        description
+        keywords
+      }
+    }
+  }
+`
