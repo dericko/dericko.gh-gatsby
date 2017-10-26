b4031008b6c96bb6333e2343bd1751fe54c4c4ca
Derick Olson <olsonderick@gmail.com>
2017-10-26 02:36:49 -0400

    update landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-export default () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>This is a website</p>
-    <Link to="/writing">Writing</Link>
-    <br />
-    <Link to="/software">Software</Link>
+import profilePhoto from '../assets/profile-photo.jpg'
+
+import styles from './styles/index.module.scss'
+
+export default ({ data }) => (
+  <div className={styles.container}>
+    <section className={styles.intro}>
+      <img src={profilePhoto} alt="Profile photo" />
+      <p>
+        Hi there, my name's Derick. I write fiction and creative non-fiction. I
+        also offer freelance software services.
+      </p>
+    </section>
+    <section className={styles.links}>
+      <Link className={styles.link} to="/writing">
+        Writing
+      </Link>
+      <Link className={styles.link} to="/software">
+        Software
+      </Link>
+    </section>
   </div>
 )
